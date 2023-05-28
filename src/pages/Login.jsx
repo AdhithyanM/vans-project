021ead1ba44c3ee9fe164d5056a078a248544546
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-05-28 21:48:01 +0530

    Protected Routes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+
+import { loginUser } from "../api";
+
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
 
 const Login = () => {
+  const message = useLoaderData();
+  const navigate = useNavigate();
   const [loginFormData, setLoginFormData] = useState({
     email: "",
     password: "",
   });
+  const [formStatus, setFormStatus] = useState("idle");
+  const [error, setError] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginFormData);
+    setFormStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => {
+        navigate("/host", { replace: true }); // replace is set to true so that when user clicks back, it won't show login form again
+      })
+      .catch((error) => setError(error))
+      .finally(() => {
+        setFormStatus("idle");
+      });
   }
 
   function handleChange(e) {
@@ -22,6 +42,8 @@ const Login = () => {
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
+      {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -37,7 +59,9 @@ const Login = () => {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={formStatus === "submitting"}>
+          {formStatus === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -26,33 +26,61 @@ import HostVanPhotos from "../pages/Host/Vans/VanPhotos";
 import NotFound from "../pages/NotFound";
 
 // LOADERS IMPORT
+import { loader as loginLoader } from "../pages/Login";
 import { loader as vansLoader } from "../pages/Vans/Vans";
 import { loader as vanDetailLoader } from "../pages/Vans/VanDetail";
 import { loader as hostVansLoader } from "../pages/Host/Vans/Vans";
 import { loader as hostVanDetailLoader } from "../pages/Host/Vans/VanDetail";
+
+// OTHER IMPORTS
+import { requireAuth } from "../utils";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<Error />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} />
+      <Route path="login" element={<Login />} loader={loginLoader} />
       <Route path="vans" element={<Vans />} loader={vansLoader} />
       <Route path="vans/:id" element={<VanDetail />} loader={vanDetailLoader} />
 
       <Route path="host" element={<HostLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="income" element={<Income />} />
-        <Route path="reviews" element={<Reviews />} />
+        <Route
+          index
+          element={<Dashboard />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="income"
+          element={<Income />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="reviews"
+          element={<Reviews />}
+          loader={async () => await requireAuth()}
+        />
         <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
         <Route
           path="vans/:id"
           element={<HostVanDetail />}
           loader={hostVanDetailLoader}
         >
-          <Route index element={<HostVanInfo />} />
-          <Route path="pricing" element={<HostVanPricing />} />
-          <Route path="photos" element={<HostVanPhotos />} />
+          <Route
+            index
+            element={<HostVanInfo />}
+            loader={async () => await requireAuth()}
+          />
+          <Route
+            path="pricing"
+            element={<HostVanPricing />}
+            loader={async () => await requireAuth()}
+          />
+          <Route
+            path="photos"
+            element={<HostVanPhotos />}
+            loader={async () => await requireAuth()}
+          />
         </Route>
       </Route>
 
